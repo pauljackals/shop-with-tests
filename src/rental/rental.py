1eb0ef7c8f6f7c2f3b848c35c3d9c545b6ca8965
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-27 19:56:36 +0100

    added check for database filename type

--- src/rental/rental.py
+++ src/rental/rental.py
@@ -7,6 +7,8 @@ class Rental:
         self.database = {}
 
     def load_database(self, database_file='database.json'):
+        if type(database_file) != str:
+            raise TypeError('Database file name must be a string')
         if not os.path.exists('../data/'+database_file):
             raise FileNotFoundError("Database doesn't exist")
         with open('../data/' + database_file) as file:
