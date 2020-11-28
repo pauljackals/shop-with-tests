d9c371963fa5a6e37f0039752af4d033cb80f7e8
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-28 17:38:02 +0100

    fixed a test

--- tests/test_rental_assertpy.py
+++ tests/test_rental_assertpy.py
@@ -52,14 +52,12 @@ class TestRentalAssertPy(unittest.TestCase):
         ).is_instance_of(uuid.UUID)
 
     def test_create_reservation_wrong_date_from_non_digit(self):
-        assert_that(
-            self.rental.create_reservation(
-                '8a85f066-bd8d-43df-b471-a6e708471c4c',
-                1,
-                '20d0-12-19 14:30',
-                '2020-12-21 13:00'
-            )
-        ).raises(ValueError)
+        assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
+            '8a85f066-bd8d-43df-b471-a6e708471c4c',
+            1,
+            '20d0-12-19 14:30',
+            '2020-12-21 13:00'
+        )
 
     def tearDown(self):
         self.rental = None
