e33645e1d134038e3c3ac4348e2d5ffb359c744f
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-28 19:19:38 +0100

    testing wrong non leap february day in end date

--- tests/test_rental_assertpy.py
+++ tests/test_rental_assertpy.py
@@ -91,6 +91,14 @@ class TestRentalAssertPy(unittest.TestCase):
             '2021-12-21 13:00'
         )
 
+    def test_create_reservation_wrong_date_to_wrong_day_in_month_february_non_leap(self):
+        assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
+            '8a85f066-bd8d-43df-b471-a6e708471c4c',
+            1,
+            '2021-02-21 14:30',
+            '2021-02-29 13:00'
+        )
+
     def test_create_reservation_from_day_in_month_february_leap(self):
         self.rental.load_database()
         assert_that(
--- tests/test_rental_pyhamcrest.py
+++ tests/test_rental_pyhamcrest.py
@@ -107,6 +107,17 @@ class TestRentalPyHamcrest(unittest.TestCase):
             raises(ValueError)
         )
 
+    def test_create_reservation_wrong_date_to_wrong_day_in_month_february_non_leap(self):
+        assert_that(
+            calling(self.rental.create_reservation).with_args(
+                '8a85f066-bd8d-43df-b471-a6e708471c4c',
+                1,
+                '2021-12-21 14:30',
+                '2021-02-29 13:00'
+            ),
+            raises(ValueError)
+        )
+
     def test_create_reservation_from_day_in_month_february_leap(self):
         self.rental.load_database()
         assert_that(
