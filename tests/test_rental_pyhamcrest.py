3f2431232f3e354d09f019cd57d2c1da5e040719
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-28 19:21:12 +0100

    testing leap february day in end date

--- tests/test_rental_pyhamcrest.py
+++ tests/test_rental_pyhamcrest.py
@@ -130,6 +130,18 @@ class TestRentalPyHamcrest(unittest.TestCase):
             instance_of(uuid.UUID)
         )
 
+    def test_create_reservation_to_day_in_month_february_leap(self):
+        self.rental.load_database()
+        assert_that(
+            uuid.UUID(self.rental.create_reservation(
+                '8a85f066-bd8d-43df-b471-a6e708471c4c',
+                1,
+                '2024-02-21 14:30',
+                '2024-02-29 13:00'
+            ), version=4),
+            instance_of(uuid.UUID)
+        )
+
     def tearDown(self):
         self.rental = None
 
