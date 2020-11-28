dcabf9860698b4766a13936d773d01af66d24778
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-28 20:03:25 +0100

    testing to date 30 minute round

--- tests/test_rental_assertpy.py
+++ tests/test_rental_assertpy.py
@@ -143,6 +143,14 @@ class TestRentalAssertPy(unittest.TestCase):
             '2020-12-21 13:00'
         )
 
+    def test_create_reservation_minute_error_date_to(self):
+        assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
+            '8a85f066-bd8d-43df-b471-a6e708471c4c',
+            1,
+            '2020-12-19 14:30',
+            '2020-12-21 13:01'
+        )
+
     def tearDown(self):
         self.rental = None
 
