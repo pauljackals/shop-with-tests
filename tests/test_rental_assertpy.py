9e138d8963715d883646b6996d805432f660532e
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-28 17:30:58 +0100

    testing reservation start date validity (changed them a bit)

--- tests/test_rental_assertpy.py
+++ tests/test_rental_assertpy.py
@@ -51,12 +51,12 @@ class TestRentalAssertPy(unittest.TestCase):
             ), version=4)
         ).is_instance_of(uuid.UUID)
 
-    def test_create_reservation_wrong_date_from(self):
+    def test_create_reservation_wrong_date_from_non_digit(self):
         assert_that(
             self.rental.create_reservation(
                 '8a85f066-bd8d-43df-b471-a6e708471c4c',
                 1,
-                '2020-gt-19 14:30',
+                '20d0-12-19 14:30',
                 '2020-12-21 13:00'
             )
         ).raises(ValueError)
--- tests/test_rental_pyhamcrest.py
+++ tests/test_rental_pyhamcrest.py
@@ -52,12 +52,12 @@ class TestRentalPyHamcrest(unittest.TestCase):
             instance_of(uuid.UUID)
         )
 
-    def test_create_reservation_wrong_date_from(self):
+    def test_create_reservation_wrong_date_from_non_digit(self):
         assert_that(
             calling(self.rental.create_reservation).with_args(
                 '8a85f066-bd8d-43df-b471-a6e708471c4c',
                 1,
-                '2020-gt-19 14:30',
+                '20d0-12-19 14:30',
                 '2020-12-21 13:00'
             ),
             raises(ValueError)
