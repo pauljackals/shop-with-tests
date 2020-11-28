b8ca6cf454547503961a667aef007da74b16ea6f
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-28 15:09:49 +0100

    testing getting user's reservations

--- tests/test_rental_assertpy.py
+++ tests/test_rental_assertpy.py
@@ -19,6 +19,19 @@ class TestRentalAssertPy(unittest.TestCase):
     def test_save_database(self):
         assert_that(self.rental.save_database()).is_true()
 
+    def test_get_user_reservations(self):
+        reservations = [
+            {
+                "id": "4248797f-9a3e-4a52-b3f7-bb72eef51755",
+                "user": "2fe45694-eb13-4283-824e-cd6fb179bfcf",
+                "game": 1,
+                "from": "2020-12-15 13:00",
+                "to": "2020-12-19 14:30"
+            }
+        ]
+        self.rental.load_database()
+        assert_that(self.rental.get_user_reservations('2fe45694-eb13-4283-824e-cd6fb179bfcf')).contains_only(*reservations)
+
     def tearDown(self):
         self.rental = None
 
