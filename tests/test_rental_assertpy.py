9b90464ad60fab7d6cf69cdb7e6cbdb857e6e147
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-28 23:11:46 +0100

    testing reservation creation with end date during another reservation

--- tests/test_rental_assertpy.py
+++ tests/test_rental_assertpy.py
@@ -215,6 +215,14 @@ class TestRentalAssertPy(unittest.TestCase):
             '2020-12-21 15:00'
         )
 
+    def test_create_reservation_error_date_to_already_taken(self):
+        assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
+            '8a85f066-bd8d-43df-b471-a6e708471c4c',
+            1,
+            '2020-12-14 13:30',
+            '2020-12-16 15:00'
+        )
+
     def tearDown(self):
         self.rental = None
 
