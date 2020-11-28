f2aeaa1db43933ba497ea21ffa04d9b59e357bdd
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-28 22:10:36 +0100

    testing reservation creation end date before open hours

--- tests/test_rental_pyhamcrest.py
+++ tests/test_rental_pyhamcrest.py
@@ -239,6 +239,17 @@ class TestRentalPyHamcrest(unittest.TestCase):
             raises(ValueError)
         )
 
+    def test_create_reservation_error_date_to_open_hours_before(self):
+        assert_that(
+            calling(self.rental.create_reservation).with_args(
+                '8a85f066-bd8d-43df-b471-a6e708471c4c',
+                1,
+                '2020-12-18 14:30',
+                '2020-12-19 09:00'
+            ),
+            raises(ValueError)
+        )
+
     def tearDown(self):
         self.rental = None
 
