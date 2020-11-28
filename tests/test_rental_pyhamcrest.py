f13b8b75933bf2e62b6bb6a692a3beee4966c4dc
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-28 18:41:32 +0100

    testing non-digit date in end date

--- tests/test_rental_pyhamcrest.py
+++ tests/test_rental_pyhamcrest.py
@@ -63,6 +63,17 @@ class TestRentalPyHamcrest(unittest.TestCase):
             raises(ValueError)
         )
 
+    def test_create_reservation_wrong_date_to_non_digit(self):
+        assert_that(
+            calling(self.rental.create_reservation).with_args(
+                '8a85f066-bd8d-43df-b471-a6e708471c4c',
+                1,
+                '2020-12-19 14:30',
+                '20d0-12-21 13:00'
+            ),
+            raises(ValueError)
+        )
+
     def test_create_reservation_wrong_date_from_wrong_day_in_month(self):
         assert_that(
             calling(self.rental.create_reservation).with_args(
