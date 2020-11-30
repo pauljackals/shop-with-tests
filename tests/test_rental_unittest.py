07efef687b25f40139a2824041336adfde2113db
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-30 22:50:40 +0100

    testing reservation creation start date in february (leap year)

--- tests/test_rental_unittest.py
+++ tests/test_rental_unittest.py
@@ -116,6 +116,15 @@ class TestRentalUnittest(unittest.TestCase):
                 '2021-02-29 13:00'
             )
 
+    def test_create_reservation_from_day_in_month_february_leap(self):
+        self.assertIsInstance(
+            uuid.UUID(self.rental.create_reservation(
+                '8a85f066-bd8d-43df-b471-a6e708471c4c',
+                1,
+                '2024-02-29 14:30',
+                '2024-12-21 13:00'
+            ), version=4), uuid.UUID)
+
     def tearDown(self):
         self.rental = None
 
