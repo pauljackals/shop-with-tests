614096aa9f9fd75d6e067e30e427bf926e3fa3bf
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-30 21:59:39 +0100

    moved databases to proper places

--- src/rental/rental.py
+++ src/rental/rental.py
@@ -15,14 +15,14 @@ class Rental:
     def load_database(self, database_file='database.json'):
         if type(database_file) != str:
             raise TypeError('Database file name must be a string')
-        if not os.path.exists('../data/'+database_file):
+        if not os.path.exists('rental/'+database_file):
             raise FileNotFoundError("Database doesn't exist")
-        with open('../data/' + database_file) as file:
+        with open('rental/' + database_file) as file:
             self._database = json.loads(file.read())
         return True
 
     def save_database(self):
-        with open('../data/database_copy.json', 'w') as file:
+        with open('rental/database_copy.json', 'w') as file:
             file.write(json.dumps(self._database))
         return True
 
