74914f961d232a6b1b4cad1f3c8ddd21f1104074
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-30 22:47:55 +0100

    testing reservation creation start date with wrong day number in february (non-leap year)

--- tests/test_rental_unittest.py
+++ tests/test_rental_unittest.py
@@ -98,6 +98,15 @@ class TestRentalUnittest(unittest.TestCase):
                 '2021-04-31 13:00'
             )
 
+    def test_create_reservation_wrong_date_from_wrong_day_in_month_february_non_leap(self):
+        with self.assertRaises(ValueError):
+            self.rental.create_reservation(
+                '8a85f066-bd8d-43df-b471-a6e708471c4c',
+                1,
+                '2021-02-29 14:30',
+                '2021-12-21 13:00'
+            )
+
     def tearDown(self):
         self.rental = None
 
