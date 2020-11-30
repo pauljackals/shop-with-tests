19119fdf51656120fd045c20f73bff9636d9bca7
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-30 22:42:05 +0100

    testing reservation creation with non-digit end date

--- tests/test_rental_unittest.py
+++ tests/test_rental_unittest.py
@@ -71,6 +71,15 @@ class TestRentalUnittest(unittest.TestCase):
                 '2020-12-21 13:00'
             )
 
+    def test_create_reservation_wrong_date_to_non_digit(self):
+        with self.assertRaises(ValueError):
+            self.rental.create_reservation(
+                '8a85f066-bd8d-43df-b471-a6e708471c4c',
+                1,
+                '2020-12-19 14:30',
+                '20d0-12-21 13:00'
+            )
+
     def tearDown(self):
         self.rental = None
 
