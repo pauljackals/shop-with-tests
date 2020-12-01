6fb3f5accde351348507b3e32c7ffcb0613774ce
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-01 19:50:10 +0100

    testing reservation creation start date with wrong day in month

--- tests/test_rental_paramererized_outside.py
+++ tests/test_rental_paramererized_outside.py
@@ -16,6 +16,12 @@ from rental.rental import Rental
             1,
             '2020-12-19 14:30',
             '20d0-12-21 13:00'
+    ), ValueError),
+    ('wrong_date_from_wrong_day_in_month', (
+            '8a85f066-bd8d-43df-b471-a6e708471c4c',
+            1,
+            '2020-11-31 14:30',
+            '2020-12-21 13:00'
     ), ValueError)
 ])
 def test_create_reservation(name, data, error):
