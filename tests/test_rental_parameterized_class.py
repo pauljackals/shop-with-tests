da27d12e312d8d0b4b50c3fc09ed7410ba3688a6
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-01 21:25:31 +0100

    testing reservation creation with start date later than end date

---
+++ tests/test_rental_parameterized_class.py
@@ -0,0 +1,32 @@
+import unittest
+from parameterized import parameterized_class
+import json
+from rental.rental import Rental
+
+
+@parameterized_class(
+    ('name', 'data', 'error'), [
+        ('error_dates_switched', (
+            '8a85f066-bd8d-43df-b471-a6e708471c4c',
+            1,
+            '2020-12-21 13:00',
+            '2020-12-19 14:30'
+        ), ValueError)
+    ]
+)
+class TestRentalParameterizedClass(unittest.TestCase):
+    def setUp(self):
+        with open('data/database_for_testing.json') as file:
+            database = json.loads(file.read())
+        self.rental = Rental(database)
+
+    def test_create_reservation(self):
+        with self.assertRaises(self.error):
+            self.rental.create_reservation(*self.data)
+
+    def tearDown(self):
+        self.rental = None
+
+
+if __name__ == '__main__':
+    unittest.main()
