c903c3000753e8503d7c4cf4b853ee95e51c19d9
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-01 21:27:50 +0100

    testing reservation creation with end date before open hours

--- tests/test_rental_parameterized_class.py
+++ tests/test_rental_parameterized_class.py
@@ -35,6 +35,12 @@ from rental.rental import Rental
             1,
             '2020-12-18 21:00',
             '2020-12-19 13:00'
+        ), ValueError),
+        ('error_date_to_open_hours_before', (
+            '8a85f066-bd8d-43df-b471-a6e708471c4c',
+            1,
+            '2020-12-18 14:30',
+            '2020-12-19 09:00'
         ), ValueError)
     ]
 )
