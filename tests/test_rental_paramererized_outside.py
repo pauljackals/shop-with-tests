7c319619cfdf7d1eb90b1f066ce88fc281de9212
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-01 19:47:44 +0100

    testing reservation creation with non-digit starting date

---
+++ tests/test_rental_paramererized_outside.py
@@ -0,0 +1,19 @@
+from assertpy import assert_that
+from parameterized import parameterized
+import json
+from rental.rental import Rental
+
+
+@parameterized([
+    ('wrong_date_from_non_digit', (
+            '8a85f066-bd8d-43df-b471-a6e708471c4c',
+            1,
+            '20d0-12-19 14:30',
+            '2020-12-21 13:00'
+    ), ValueError)
+])
+def test_create_reservation(name, data, error):
+    with open('data/database_for_testing.json') as file:
+        database = json.loads(file.read())
+    rental = Rental(database)
+    assert_that(rental.create_reservation).raises(error).when_called_with(*data)
