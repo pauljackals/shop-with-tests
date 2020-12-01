6b31b0019fede1416215280807faf90274eb8f76
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-01 21:26:30 +0100

    testing reservation creation with end date during closed day

--- tests/test_rental_parameterized_class.py
+++ tests/test_rental_parameterized_class.py
@@ -17,6 +17,12 @@ from rental.rental import Rental
             1,
             '2020-12-20 14:30',
             '2020-12-22 13:00'
+        ), ValueError),
+        ('error_date_to_closed_day', (
+            '8a85f066-bd8d-43df-b471-a6e708471c4c',
+            1,
+            '2020-12-19 14:30',
+            '2020-12-20 13:00'
         ), ValueError)
     ]
 )
