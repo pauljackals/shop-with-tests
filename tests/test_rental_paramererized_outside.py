47b4ed905db09b8fe7fccd19e1fd04d6555bfe8a
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-01 19:54:34 +0100

    testing reservation creation empty start date

--- tests/test_rental_paramererized_outside.py
+++ tests/test_rental_paramererized_outside.py
@@ -40,6 +40,12 @@ from rental.rental import Rental
             1,
             '2021-02-21 14:30',
             '2021-02-29 13:00'
+    ), ValueError),
+    ('error_date_from_empty', (
+            '8a85f066-bd8d-43df-b471-a6e708471c4c',
+            1,
+            '',
+            '2020-12-21 13:00'
     ), ValueError)
 ])
 def test_create_reservation(name, data, error):
