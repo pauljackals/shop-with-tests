413cf0e38ece2356afc303c556c4093c5ef6b807
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-01 19:52:58 +0100

    testing reservation creation end date with wrong day in february (non-leap year)

--- tests/test_rental_paramererized_outside.py
+++ tests/test_rental_paramererized_outside.py
@@ -34,6 +34,12 @@ from rental.rental import Rental
             1,
             '2021-02-29 14:30',
             '2021-12-21 13:00'
+    ), ValueError),
+    ('wrong_date_to_wrong_day_in_month_february_non_leap', (
+            '8a85f066-bd8d-43df-b471-a6e708471c4c',
+            1,
+            '2021-02-21 14:30',
+            '2021-02-29 13:00'
     ), ValueError)
 ])
 def test_create_reservation(name, data, error):
