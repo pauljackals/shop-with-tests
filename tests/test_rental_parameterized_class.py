4a34f5ffc633cbe9f824c1fd71ce7c859271dcd6
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-02 23:05:54 +0100

    added messages to exception assertions in parameterized tests (class)

--- tests/test_rental_parameterized_class.py
+++ tests/test_rental_parameterized_class.py
@@ -5,61 +5,61 @@ from rental.rental import Rental
 
 
 @parameterized_class(
-    ('name', 'data', 'error'), [
+    ('name', 'data', 'error', 'message'), [
         ('error_dates_switched', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-21 13:00',
             '2020-12-19 14:30'
-        ), ValueError),
+        ), ValueError, 'End date must be later than start date'),
         ('error_date_from_closed_day', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-20 14:30',
             '2020-12-22 13:00'
-        ), ValueError),
+        ), ValueError, 'Rental shop is closed during this time'),
         ('error_date_to_closed_day', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-19 14:30',
             '2020-12-20 13:00'
-        ), ValueError),
+        ), ValueError, 'Rental shop is closed during this time'),
         ('error_date_from_open_hours_before', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-18 08:30',
             '2020-12-19 13:00'
-        ), ValueError),
+        ), ValueError, 'Rental shop is closed during this time'),
         ('error_date_from_open_hours_after', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-18 21:00',
             '2020-12-19 13:00'
-        ), ValueError),
+        ), ValueError, 'Rental shop is closed during this time'),
         ('error_date_to_open_hours_before', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-18 14:30',
             '2020-12-19 09:00'
-        ), ValueError),
+        ), ValueError, 'Rental shop is closed during this time'),
         ('error_date_to_open_hours_after', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-18 14:00',
             '2020-12-19 16:00'
-        ), ValueError),
+        ), ValueError, 'Rental shop is closed during this time'),
         ('error_date_from_already_taken', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-15 13:30',
             '2020-12-21 15:00'
-        ), ValueError),
+        ), ValueError, 'Game is already reserved during this time'),
         ('error_date_to_already_taken', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-14 13:30',
             '2020-12-16 15:00'
-        ), ValueError)
+        ), ValueError, 'Game is already reserved during this time')
     ]
 )
 class TestRentalParameterizedClass(unittest.TestCase):
@@ -69,7 +69,7 @@ class TestRentalParameterizedClass(unittest.TestCase):
         self.rental = Rental(database)
 
     def test_create_reservation(self):
-        with self.assertRaises(self.error):
+        with self.assertRaisesRegex(self.error, '^' + self.message + '$'):
             self.rental.create_reservation(*self.data)
 
     def tearDown(self):
