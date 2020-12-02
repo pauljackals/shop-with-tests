a5d36d6ea53fbc403bb1761f222a123de588b8cf
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-02 23:02:28 +0100

    added messages to exception assertions in parameterized tests (outside class)

--- tests/test_rental_paramererized_outside.py
+++ tests/test_rental_paramererized_outside.py
@@ -10,94 +10,94 @@ from rental.rental import Rental
             1,
             '20d0-12-19 14:30',
             '2020-12-21 13:00'
-    ), ValueError),
+    ), ValueError, 'Wrong date syntax'),
     ('wrong_date_to_non_digit', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-19 14:30',
             '20d0-12-21 13:00'
-    ), ValueError),
+    ), ValueError, 'Wrong date syntax'),
     ('wrong_date_from_wrong_day_in_month', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-11-31 14:30',
             '2020-12-21 13:00'
-    ), ValueError),
+    ), ValueError, 'No such day in provided month'),
     ('wrong_date_to_wrong_day_in_month', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2021-04-21 14:30',
             '2021-04-31 13:00'
-    ), ValueError),
+    ), ValueError, 'No such day in provided month'),
     ('wrong_date_from_wrong_day_in_month_february_non_leap', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2021-02-29 14:30',
             '2021-12-21 13:00'
-    ), ValueError),
+    ), ValueError, 'No such day in provided month'),
     ('wrong_date_to_wrong_day_in_month_february_non_leap', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2021-02-21 14:30',
             '2021-02-29 13:00'
-    ), ValueError),
+    ), ValueError, 'No such day in provided month'),
     ('error_date_from_empty', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '',
             '2020-12-21 13:00'
-    ), ValueError),
+    ), ValueError, 'Wrong date syntax'),
     ('error_date_to_empty', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-19 14:30',
             ''
-    ), ValueError),
+    ), ValueError, 'Wrong date syntax'),
     ('wrong_user_type', (
             34,
             1,
             '2020-12-19 14:30',
             '2020-12-21 13:00'
-    ), TypeError),
+    ), TypeError, 'User ID must be a string'),
     ('no_user', (
             'test',
             1,
             '2020-12-19 14:30',
             '2020-12-21 13:00'
-    ), LookupError),
+    ), LookupError, 'No such user'),
     ('error_wrong_game_type', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             '1',
             '2020-12-19 14:30',
             '2020-12-21 13:00'
-    ), TypeError),
+    ), TypeError, 'Game ID must be an integer'),
     ('error_empty_user', (
             '',
             1,
             '2020-12-19 14:30',
             '2020-12-21 13:00'
-    ), ValueError),
+    ), ValueError, 'User ID must not be empty'),
     ('error_no_game', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             999,
             '2020-12-19 14:30',
             '2020-12-21 13:00'
-    ), LookupError),
+    ), LookupError, 'No such game'),
     ('minute_error_date_from', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-19 14:29',
             '2020-12-21 13:00'
-    ), ValueError),
+    ), ValueError, 'Both dates must be rounded to full hours or half (:00/:30)'),
     ('minute_error_date_to', (
             '8a85f066-bd8d-43df-b471-a6e708471c4c',
             1,
             '2020-12-19 14:30',
             '2020-12-21 13:01'
-    ), ValueError)
+    ), ValueError, 'Both dates must be rounded to full hours or half (:00/:30)')
 ])
-def test_parameterized_create_reservation(name, data, error):
+def test_parameterized_create_reservation(name, data, error, message):
     with open('data/database_for_testing.json') as file:
         database = json.loads(file.read())
     rental = Rental(database)
-    assert_that(rental.create_reservation).raises(error).when_called_with(*data)
+    assert_that(rental.create_reservation).raises(error).when_called_with(*data).is_equal_to(message)
