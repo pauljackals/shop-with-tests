7d2441e8f42484be490da6832ba76ad4967ab3dd
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-02 22:54:22 +0100

    added messages to exception assertions in assertpy tests

--- tests/test_rental_assertpy.py
+++ tests/test_rental_assertpy.py
@@ -16,13 +16,13 @@ class TestRentalAssertPy(unittest.TestCase):
         assert_that(self.rental.load_database('data/database_for_testing.json')).is_true()
 
     def test_load_database_no_file(self):
-        assert_that(self.rental.load_database).raises(FileNotFoundError).when_called_with('test')
+        assert_that(self.rental.load_database).raises(FileNotFoundError).when_called_with('test').is_equal_to("Database doesn't exist")
 
     def test_load_database_wrong_type(self):
-        assert_that(self.rental.load_database).raises(TypeError).when_called_with(23)
+        assert_that(self.rental.load_database).raises(TypeError).when_called_with(23).is_equal_to('Database file name must be a string')
 
     def test_load_database_empty_name(self):
-        assert_that(self.rental.load_database).raises(ValueError).when_called_with('')
+        assert_that(self.rental.load_database).raises(ValueError).when_called_with('').is_equal_to('Database file name must not be empty')
 
     def test_save_database(self):
         assert_that(self.rental.save_database()).is_true()
@@ -46,13 +46,13 @@ class TestRentalAssertPy(unittest.TestCase):
         assert_that(self.rental.get_user_reservations('2fe45694-eb13-4283-824e-cd6fb179bfcf')).contains_only(*reservations)
 
     def test_get_user_reservations_wrong_type(self):
-        assert_that(self.rental.get_user_reservations).raises(TypeError).when_called_with(123)
+        assert_that(self.rental.get_user_reservations).raises(TypeError).when_called_with(123).is_equal_to('User ID must be a string')
 
     def test_get_user_reservations_empty(self):
-        assert_that(self.rental.get_user_reservations).raises(ValueError).when_called_with('')
+        assert_that(self.rental.get_user_reservations).raises(ValueError).when_called_with('').is_equal_to('User ID must not be empty')
 
     def test_get_user_reservations_no_user(self):
-        assert_that(self.rental.get_user_reservations).raises(LookupError).when_called_with('test')
+        assert_that(self.rental.get_user_reservations).raises(LookupError).when_called_with('test').is_equal_to('No such user')
 
     def test_create_reservation(self):
         assert_that(
@@ -70,7 +70,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '20d0-12-19 14:30',
             '2020-12-21 13:00'
-        )
+        ).is_equal_to('Wrong date syntax')
 
     def test_create_reservation_wrong_date_to_non_digit(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -78,7 +78,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-19 14:30',
             '20d0-12-21 13:00'
-        )
+        ).is_equal_to('Wrong date syntax')
 
     def test_create_reservation_wrong_date_from_wrong_day_in_month(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -86,7 +86,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-11-31 14:30',
             '2020-12-21 13:00'
-        )
+        ).is_equal_to('No such day in provided month')
 
     def test_create_reservation_wrong_date_to_wrong_day_in_month(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -94,7 +94,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2021-04-21 14:30',
             '2021-04-31 13:00'
-        )
+        ).is_equal_to('No such day in provided month')
 
     def test_create_reservation_wrong_date_from_wrong_day_in_month_february_non_leap(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -102,7 +102,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2021-02-29 14:30',
             '2021-12-21 13:00'
-        )
+        ).is_equal_to('No such day in provided month')
 
     def test_create_reservation_wrong_date_to_wrong_day_in_month_february_non_leap(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -110,7 +110,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2021-02-21 14:30',
             '2021-02-29 13:00'
-        )
+        ).is_equal_to('No such day in provided month')
 
     def test_create_reservation_from_day_in_month_february_leap(self):
         assert_that(
@@ -138,7 +138,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '',
             '2020-12-21 13:00'
-        )
+        ).is_equal_to('Wrong date syntax')
 
     def test_create_reservation_error_date_to_empty(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -146,7 +146,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-19 14:30',
             ''
-        )
+        ).is_equal_to('Wrong date syntax')
 
     def test_create_reservation_wrong_user_type(self):
         assert_that(self.rental.create_reservation).raises(TypeError).when_called_with(
@@ -154,7 +154,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-19 14:30',
             '2020-12-21 13:00'
-        )
+        ).is_equal_to('User ID must be a string')
 
     def test_create_reservation_no_user(self):
         assert_that(self.rental.create_reservation).raises(LookupError).when_called_with(
@@ -162,7 +162,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-19 14:30',
             '2020-12-21 13:00'
-        )
+        ).is_equal_to('No such user')
 
     def test_create_reservation_error_wrong_game_type(self):
         assert_that(self.rental.create_reservation).raises(TypeError).when_called_with(
@@ -170,7 +170,7 @@ class TestRentalAssertPy(unittest.TestCase):
             '1',
             '2020-12-19 14:30',
             '2020-12-21 13:00'
-        )
+        ).is_equal_to('Game ID must be an integer')
 
     def test_create_reservation_error_empty_user(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -178,7 +178,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-19 14:30',
             '2020-12-21 13:00'
-        )
+        ).is_equal_to('User ID must not be empty')
 
     def test_create_reservation_error_no_game(self):
         assert_that(self.rental.create_reservation).raises(LookupError).when_called_with(
@@ -186,7 +186,7 @@ class TestRentalAssertPy(unittest.TestCase):
             999,
             '2020-12-19 14:30',
             '2020-12-21 13:00'
-        )
+        ).is_equal_to('No such game')
 
     def test_create_reservation_minute_error_date_from(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -194,7 +194,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-19 14:29',
             '2020-12-21 13:00'
-        )
+        ).is_equal_to('Both dates must be rounded to full hours or half (:00/:30)')
 
     def test_create_reservation_minute_error_date_to(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -202,7 +202,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-19 14:30',
             '2020-12-21 13:01'
-        )
+        ).is_equal_to('Both dates must be rounded to full hours or half (:00/:30)')
 
     def test_create_reservation_error_dates_switched(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -210,7 +210,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-21 13:00',
             '2020-12-19 14:30'
-        )
+        ).is_equal_to('End date must be later than start date')
 
     def test_create_reservation_error_date_from_closed_day(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -218,7 +218,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-20 14:30',
             '2020-12-22 13:00'
-        )
+        ).is_equal_to('Rental shop is closed during this time')
 
     def test_create_reservation_error_date_to_closed_day(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -226,7 +226,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-19 14:30',
             '2020-12-20 13:00'
-        )
+        ).is_equal_to('Rental shop is closed during this time')
 
     def test_create_reservation_error_date_from_open_hours_before(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -234,7 +234,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-18 08:30',
             '2020-12-19 13:00'
-        )
+        ).is_equal_to('Rental shop is closed during this time')
 
     def test_create_reservation_error_date_from_open_hours_after(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -242,7 +242,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-18 21:00',
             '2020-12-19 13:00'
-        )
+        ).is_equal_to('Rental shop is closed during this time')
 
     def test_create_reservation_error_date_to_open_hours_before(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -250,7 +250,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-18 14:30',
             '2020-12-19 09:00'
-        )
+        ).is_equal_to('Rental shop is closed during this time')
 
     def test_create_reservation_error_date_to_open_hours_after(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -258,7 +258,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-18 14:00',
             '2020-12-19 16:00'
-        )
+        ).is_equal_to('Rental shop is closed during this time')
 
     def test_create_reservation_error_date_from_already_taken(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -266,7 +266,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-15 13:30',
             '2020-12-21 15:00'
-        )
+        ).is_equal_to('Game is already reserved during this time')
 
     def test_create_reservation_error_date_to_already_taken(self):
         assert_that(self.rental.create_reservation).raises(ValueError).when_called_with(
@@ -274,7 +274,7 @@ class TestRentalAssertPy(unittest.TestCase):
             1,
             '2020-12-14 13:30',
             '2020-12-16 15:00'
-        )
+        ).is_equal_to('Game is already reserved during this time')
 
     def test_add_user(self):
         assert_that(
@@ -290,28 +290,28 @@ class TestRentalAssertPy(unittest.TestCase):
             'Test',
             '',
             'something@example.com'
-        )
+        ).is_equal_to('Names must not be empty')
 
     def test_add_user_error_wrong_name_type(self):
         assert_that(self.rental.add_user).raises(TypeError).when_called_with(
             1,
             'Testington',
             'something@example.com'
-        )
+        ).is_equal_to('Names must be strings')
 
     def test_add_user_error_wrong_email_type(self):
         assert_that(self.rental.add_user).raises(TypeError).when_called_with(
             'Test',
             'Testington',
             None
-        )
+        ).is_equal_to('Email must be a string')
 
     def test_add_user_error_email_invalid(self):
         assert_that(self.rental.add_user).raises(ValueError).when_called_with(
             'Test',
             'Testington',
             'somethingexample.com'
-        )
+        ).is_equal_to('Email is not valid')
 
     def test_get_stats(self):
         assert_that(self.rental.get_stats()).is_equal_to(
