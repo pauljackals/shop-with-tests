48c6a5c9bdb22689bad9f83ad17c926592219eb8
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-03 12:54:01 +0100

    testing getting all data from new class User

---
+++ tests/test_user_unittest.py
@@ -0,0 +1,27 @@
+import unittest
+from rental.user import User
+
+
+class TestUserUnittest(unittest.TestCase):
+    def setUp(self):
+        data = {
+          "id": "8a85f066-bd8d-43df-b471-a6e708471c4c",
+          "email": "test@example.com",
+          "name": {
+            "first": "John",
+            "last": "Doe"
+          }
+        }
+        self.user = User(data)
+        self.data = data
+
+    def test_get_all_data(self):
+        self.assertDictEqual(self.user.get_all_data(), self.data)
+
+    def tearDown(self):
+        self.user = None
+        self.data = None
+
+
+if __name__ == '__main__':
+    unittest.main()
