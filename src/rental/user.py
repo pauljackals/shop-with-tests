95ba2653f224563873d44f9181c6498934fd267d
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-03 12:54:53 +0100

    added getting all user data

---
+++ src/rental/user.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, data):
+        self._id = data['id']
+        self._email = data['email']
+        self._name = data['name']
+
+    def get_all_data(self):
+        return {
+            'id': self._id,
+            'email': self._email,
+            'name': self._name
+        }
